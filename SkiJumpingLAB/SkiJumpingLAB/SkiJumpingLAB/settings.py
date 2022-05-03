3dcbdca4df259ed8f0dd15ea2b6bd7d0e55842ad
Jakub Krajewski <jakub.j.krajewski@gmail.com>
2022-05-03 12:18:41 +0100

    added more protection

--- SkiJumpingLAB/SkiJumpingLAB/SkiJumpingLAB/settings.py
+++ SkiJumpingLAB/SkiJumpingLAB/SkiJumpingLAB/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,10 +21,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-uvft3r9d)3v^#7uzy%rv#@9)*=!a@vfyno5l4m&f%_v!a@(6c&'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config('DEBUG', cast=bool)
 
 ALLOWED_HOSTS = []
 
